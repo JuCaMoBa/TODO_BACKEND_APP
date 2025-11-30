6768f2265341ecf87947f600fae7e29511bac093
Juan C. Moreno B. <juan.carlos.moreno.banda@hotmail.com>
2025-11-30 14:09:08 -0500

    fix: ajusta schemas de validacion

--- api/v1/schemas/users/user_login.py
+++ api/v1/schemas/users/user_login.py
@@ -1,11 +1,9 @@
 """Modulo que genera el modelo de datos para el login de usuarios"""
 
-from typing import Optional
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field
 
 
 class UserLogin(BaseModel):
     """Modelo de datos para el login de un usuario"""
-    username: Optional[str] = Field(..., title="Nombre de usuario", max_length=50)
-    email: Optional[EmailStr] = Field(..., title="Correo electronico", max_length=100)
-    password: str = Field(..., title="Contrasena", min_length=8, max_length=128)
+    email_or_username: str = Field(..., title="Email o Nombre de usuario")
+    password: str = Field(..., title="Contrasena")
--- api/v1/schemas/users/user_message_response.py
+++ api/v1/schemas/users/user_message_response.py
@@ -1,10 +1,12 @@
 """Modulo ue genera la respuesta cuando se crea un usuario"""
 
+from typing import Any, Optional
 from pydantic import BaseModel, Field
 
 
 class UserMessageResponse(BaseModel):
     """Modelo de datos para la respuesta de un usuario"""
-
+    success: bool = Field(..., title="Indica si la operacion fue exitosa")
+    data: Optional[Any] = Field(None, title="Datos del usuario creado")
     message: str = Field(..., title="Mensaje de respuesta")
     status: int = Field(..., title="Codigo de estado HTTP")
