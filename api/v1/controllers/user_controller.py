ea033fb4deacc7c79c6ba4d0e6d614277d2ea3c3
Juan C. Moreno B. <juan.carlos.moreno.banda@hotmail.com>
2025-11-30 14:11:42 -0500

    fix: ajusta controller de users

--- api/v1/controllers/user_controller.py
+++ api/v1/controllers/user_controller.py
@@ -1,6 +1,7 @@
 """Modulo que genera los controllers para la gestion de usuarios."""
 
-from fastapi import HTTPException
+import os
+from api.v1.schemas.users.user_login import UserLogin
 from api.v1.services.user_service import UserService
 from api.v1.schemas.users.user_create import UserCreate
 from api.v1.schemas.users.user_update import UserUpdate
@@ -10,34 +11,18 @@ class UserController:
     """Controller para la gestion de usuarios."""
 
     def __init__(self):
-        self.user_service = UserService()
+        self.user_service = UserService(os.getenv("DB_URL"))
 
     def register_user(self, user_create: UserCreate):
         """Registra un nuevo usuario."""
-        try:
-            user = self.user_service.create_user(user_create)
-            return user
-        except HTTPException as http_exc:
-            raise http_exc
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        return self.user_service.create_user(user_create)
 
-    def update_user_status(self, user_id: int, update_data: UserUpdate):
+    def update_user_status(self, update_data: UserUpdate):
         """Actualiza el estado activo de un usuario."""
-        try:
-            updated_user = self.user_service.update_user_status(user_id, update_data.is_active)
-            return updated_user
-        except HTTPException as http_exc:
-            raise http_exc
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        updated_user = self.user_service.update_user_status(update_data)
+        return updated_user
 
-    def login_user(self, user_login_data):
+    def login_user(self, user_login_data: UserLogin):
         """Inicia sesion de un usuario."""
-        try:
-            user = self.user_service.login_user(user_login_data)
-            return user
-        except HTTPException as http_exc:
-            raise http_exc
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        user = self.user_service.login_user(user_login_data)
+        return user
