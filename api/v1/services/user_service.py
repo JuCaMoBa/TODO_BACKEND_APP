fc615e667fb830102d27a01b8d331e654e348752
Juan C. Moreno B. <juan.carlos.moreno.banda@hotmail.com>
2025-11-30 14:08:18 -0500

    fix: ajusta user_service.py

--- api/v1/services/user_service.py
+++ api/v1/services/user_service.py
@@ -3,10 +3,11 @@
 from api.v1.repositories.user_repository import UserRepository
 from api.v1.schemas.users.user_create import UserCreate
 from api.v1.schemas.users.user_login import UserLogin
+from api.v1.schemas.users.user_message_response import UserMessageResponse
 from api.v1.schemas.users.user_update import UserUpdate
 import logging
-from fastapi import HTTPException
-from utils.hash_password import hash_password
+from core.global_config.exceptions.exceptions import RepositoryConnectionError
+from utils.password_managment import hash_password, verify_password
 
 
 class UserService:
@@ -19,14 +20,18 @@ class UserService:
         """Crea un nuevo usuario."""
         try:
             existing_user = self.user_repository.get_user_by_email_or_username(
-                email=user_create.email,
-                username=user_create.username
+                user_create.email
             )
             if existing_user:
                 logging.warning(
-                    f"El usuario con email {user_create.email} o username {user_create.username} ya existe."
+                    f"[Service] El usuario con email {user_create.email} o username {user_create.username} ya existe."
                 )
-                raise HTTPException(status_code=400, detail="El usuario ya existe.")
+                return UserMessageResponse(
+                    success=False,
+                    data=None,
+                    message="El usuario ya existe.",
+                    status=400
+                )
 
             hashed_password = hash_password(user_create.password)
 
@@ -36,54 +41,118 @@ class UserService:
                 hashed_password=hashed_password,
                 is_active=user_create.is_active
             )
-            logging.info(f"Usuario creado exitosamente con ID: {user_id}")
-            return user_id
-        except HTTPException:
-            raise
+            logging.info(f"[Service] Usuario creado exitosamente con ID: {user_id}")
+            return UserMessageResponse(
+                success=True,
+                data=user_id,
+                message="Usuario creado exitosamente.",
+                status=201
+            )
+        except RepositoryConnectionError as repo_exc:
+            logging.error(f"[service] Error en la base de datos al crear el usuario: {repo_exc}")
+            return UserMessageResponse(
+                success=False,
+                data=None,
+                message=str(repo_exc),
+                status=500
+            )
         except Exception as e:
-            logging.error(f"Error en el servicio al crear el usuario: {e}")
-            raise HTTPException(status_code=500, detail="Error interno al crear el usuario.")
+            logging.error(f"[service] Error inesperado al crear el usuario: {e}")
+            return UserMessageResponse(
+                success=False,
+                data=None,
+                message="Error interno al crear el usuario.",
+                status=500
+            )
 
-    def update_user_status(self, user_id: int, user_update: UserUpdate):
+    def update_user_status(self, user_update: UserUpdate):
         """Actualiza el estado activo de un usuario."""
         try:
             updated_user_id = self.user_repository.user_update_status(
-                user_id=user_id,
+                user_id=user_update.user_id,
                 is_active=user_update.is_active
             )
             if updated_user_id is None:
-                raise HTTPException(status_code=404, detail="Usuario no encontrado.")
+                logging.warning(f"[Service] Usuario con ID {user_update.user_id} no encontrado para actualizar.")
+                return UserMessageResponse(
+                    success=False,
+                    data=None,
+                    message="Usuario no encontrado.",
+                    status=404
+                )
 
-            logging.info(f"Estado del usuario con ID {user_id} actualizado exitosamente")
-            return updated_user_id
-        except HTTPException:
-            raise
+            logging.info(f"[Service] Estado del usuario con ID {user_update.user_id} actualizado exitosamente")
+
+            return UserMessageResponse(
+                success=True,
+                data=updated_user_id,
+                message="Estado del usuario actualizado exitosamente.",
+                status=200
+            )
+        except RepositoryConnectionError as repo_exc:
+            logging.error(f"[Service] Error la base de datos al actualizar el estado del usuario: {repo_exc}")
+            return UserMessageResponse(
+                success=False,
+                data=None,
+                message=str(repo_exc),
+                status=500
+            )
         except Exception as e:
-            logging.error(f"Error en el servicio al actualizar el estado del usuario: {e}")
-            raise HTTPException(status_code=500, detail="Error interno al actualizar usuario.")
+            logging.error(f"[Service] Error inesperado al actualizar el estado del usuario: {e}")
+            return UserMessageResponse(
+                success=False,
+                data=None,
+                message="Error interno al actualizar usuario.",
+                status=500
+            )
 
     def login_user(self, user_login_data: UserLogin):
         """Inicia sesion de un usuario."""
         try:
             user = self.user_repository.get_user_by_email_or_username(
-                email=user_login_data.email,
-                username=user_login_data.username
+                user_login_data.email_or_username
             )
             if not user:
-                logging.warning("Intento de inicio de sesion fallido")
-                raise HTTPException(status_code=401, detail="Credenciales invalidas.")
+                logging.warning("[Service] Intento de inicio de sesion fallido - usuario no encontrado")
+                return UserMessageResponse(
+                    success=False,
+                    data=None,
+                    message="Credenciales invalidas.",
+                    status=401
+                )
 
-            hashed_input_password = hash_password(user_login_data.password)
-            if user["hashed_password"] != hashed_input_password:
-                logging.warning("Intento de inicio de sesion fallido")
-                raise HTTPException(status_code=401, detail="Credenciales invalidas.")
+            is_password_valid = verify_password(user_login_data.password, user["hashed_password"])
+
+            if not is_password_valid:
+                logging.warning("[Service] Intento de inicio de sesion fallido - credenciales invalidas")
+                return UserMessageResponse(
+                    success=False,
+                    data=None,
+                    message="Credenciales invalidas.",
+                    status=401
+                )
 
             logging.info(
-                f"Usuario con email {user_login_data.email} o username {user_login_data.username} "
-                f"ha iniciado sesion exitosamente")
-            return user["id"]
-        except HTTPException:
-            raise
+                f"Usuario {user_login_data.email_or_username} ha iniciado sesion exitosamente")
+            return UserMessageResponse(
+                success=True,
+                data=user["id"],
+                message="Inicio de sesion exitoso.",
+                status=200
+            )
+        except RepositoryConnectionError as repo_exc:
+            logging.error(f"[Service] Error en la base de datos al iniciar sesion: {repo_exc}")
+            return UserMessageResponse(
+                success=False,
+                data=None,
+                message=str(repo_exc),
+                status=500
+            )
         except Exception as e:
-            logging.error(f"Error en el servicio al iniciar sesion: {e}")
-            raise HTTPException(status_code=500, detail="Error interno al iniciar sesion.")
+            logging.error(f"[Service] Error inesperado al iniciar sesion: {e}")
+            return UserMessageResponse(
+                success=False,
+                data=None,
+                message="Error interno al iniciar sesion.",
+                status=500
+            )
