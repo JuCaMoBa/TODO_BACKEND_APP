39e9d14507b89e0e5d558a725359e9534a424006
Juan C. Moreno B. <juan.carlos.moreno.banda@hotmail.com>
2025-12-01 14:45:55 -0500

    feat: actualiza el schema TaskMessageResponse

--- api/v1/schemas/tasks/task_message_response.py
+++ api/v1/schemas/tasks/task_message_response.py
@@ -1,11 +1,12 @@
 """Modulo que genera el modelo de datos para el mensaje de respuesta de una tarea"""
 
+from typing import Any, Optional
 from pydantic import BaseModel, Field
-from api.v1.schemas.tasks.task_response import TaskResponse
 
 
 class TaskMessageResponse(BaseModel):
     """Modelo de datos para la respuesta de una tarea"""
-    task: TaskResponse
+    success: bool = Field(..., title="Indica si la operacion fue exitosa")
+    data: Optional[Any] = Field(None, title="Datos de la tarea")
     message: str = Field(..., title="Mensaje de respuesta")
     status: int = Field(..., title="Codigo de estado HTTP")
