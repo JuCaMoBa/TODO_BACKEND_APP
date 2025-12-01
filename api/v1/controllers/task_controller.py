791ede87a49eaca12a5adfa8a7ec4c618328b5a0
Juan C. Moreno B. <juan.carlos.moreno.banda@hotmail.com>
2025-12-01 14:50:13 -0500

    feat: actualiza controller de las tasks

--- api/v1/controllers/task_controller.py
+++ api/v1/controllers/task_controller.py
@@ -1,6 +1,6 @@
 """Modulo que genera los controllers para la gestion de tareas."""
 
-from fastapi import HTTPException
+import os
 from api.v1.services.task_service import TaskService
 from api.v1.schemas.tasks.task_create import TaskCreate
 from api.v1.schemas.tasks.task_update import TaskUpdate
@@ -10,44 +10,20 @@ class TaskController:
     """Controller para la gestion de tareas."""
 
     def __init__(self):
-        self.task_service = TaskService()
+        self.task_service = TaskService(os.getenv("DB_URL"))
 
     def create_task(self, task_create: TaskCreate, user_id: int):
         """Crea una nueva tarea."""
-        try:
-            task = self.task_service.create_task(task_create, user_id)
-            return task
-        except HTTPException as http_exc:
-            raise http_exc
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        return self.task_service.create_task(task_create, user_id)
 
     def update_task(self, task_id: int, update_data: TaskUpdate, user_id: int):
         """Actualiza una tarea existente."""
-        try:
-            updated_task = self.task_service.update_task(task_id, update_data, user_id)
-            return updated_task
-        except HTTPException as http_exc:
-            raise http_exc
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        return self.task_service.update_task(task_id, update_data, user_id)
 
     def delete_task(self, task_id: int, user_id: int):
         """Elimina una tarea existente."""
-        try:
-            self.task_service.delete_task(task_id, user_id)
-            return {"detail": "Tarea eliminada exitosamente."}
-        except HTTPException as http_exc:
-            raise http_exc
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        return self.task_service.delete_task(task_id, user_id)
 
     def get_tasks(self, user_id: int):
         """Obtiene una tarea por su ID."""
-        try:
-            task = self.task_service.get_tasks(user_id)
-            return task
-        except HTTPException as http_exc:
-            raise http_exc
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+        return self.task_service.get_tasks(user_id)
