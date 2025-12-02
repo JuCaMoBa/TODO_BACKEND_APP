22c22d8aee6df192a846e1f002254b949b313a78
Juan C. Moreno B. <juan.carlos.moreno.banda@hotmail.com>
2025-12-02 10:30:07 -0500

    feat: actualiza las rutas para users

--- api/v1/routers/users_routes.py
+++ api/v1/routers/users_routes.py
@@ -2,6 +2,8 @@
 
 from fastapi import APIRouter, Depends
 from api.v1.controllers.user_controller import UserController
+from api.v1.dependency.dependencies import current_user_dependency
+from api.v1.schemas.auth.auth_token import TokenData
 from api.v1.schemas.users.user_create import UserCreate
 from api.v1.schemas.users.user_login import UserLogin
 from api.v1.schemas.users.user_message_response import UserMessageResponse
@@ -26,9 +28,11 @@ def register_user(
 @router.put("/update-status", response_model=UserMessageResponse)
 def update_status_user(
     update_data: UserUpdate,
+    current_user: TokenData = Depends(current_user_dependency),
     controller: UserController = Depends(get_user_controller)
 ):
-    data = controller.update_user_status(update_data)
+    user_id = current_user.user_id
+    data = controller.update_user_status(user_id, update_data)
     return data
 
 
